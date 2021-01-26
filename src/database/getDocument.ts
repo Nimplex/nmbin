60a79caaece3ca057137dffef6940bd904f16b42
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-26 21:18:22 +0100

    Update getDocument.ts

--- src/database/getDocument.ts
+++ src/database/getDocument.ts
@@ -1,8 +1,5 @@
 import docs, { Doc } from '../models/document.model'
 
 export default async function (id: string): Promise<Doc | null> {
-
-    const Document = await docs.findById(id)
-
-    return Document
+    return await docs.findOne({ _id: id })
 }
