92f00237f6bbc031748d8af993da7582c004dd11
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-26 21:35:33 +0100

    error prevention

--- src/database/getDocument.ts
+++ src/database/getDocument.ts
@@ -1,5 +1,15 @@
 import docs, { Doc } from '../models/document.model'
+import { Types } from 'mongoose'
 
-export default async function (id: string): Promise<Doc | null> {
-    return await docs.findOne({ _id: id })
+export default async function (id: string): Promise<Doc | undefined> {
+    let document
+
+    try {
+        document = await docs.findById(Types.ObjectId(id))
+    } catch (err) {
+        if (err) return undefined
+    }
+
+    if (!document) return undefined
+    else return document
 }
